1ed39fef52134ea1449146bbd806d50401be6dcb
Mark <ruoheng.ma@gmail.com>
2021-10-30 17:51:13 +0200

    add script for automatic quantization and compilation

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ and retrieving result back to host.
 
 ## Usage
 
-Run ```git clone --recurse-submodules https://github.com/wwMark/u96v2_vitis_ai_auto_qp_flow.git``` to clone the files.
-Then run ```quantize_compile.sh``` to start the quantization and compilation.
+Run ```git clone --recurse-submodules https://github.com/wwMark/u96v2_vitis_ai_auto_qp_flow.git``` to clone this repository.
+Then run ```quantize_compile.sh``` to start the quantization and compilation. The script will first run the python code inside ```u96v2_mobilenetv2_customcoco``` to generate custom coco dataset and then train the model. After that, it will pull and run xilinx docker image, copy all the relative files for quantization and compilation of the trained model into the docker container, run the quantization and compilation, then copy back the resulting directory containing the ```.xmodel``` file, and finally stop the container.
--- quantize_compile.sh
+++ quantize_compile.sh
@@ -1,11 +1,18 @@
+set -e
+trap "docker stop vitis_ai_quantize_compile" ERR
+
+python u96v2_mobilenetv2_customcoco/coco_processor
+python u96v2_mobilenetv2_customcoco/mobilenet_v2_trainer.py
+
 docker pull xilinx/vitis-ai:1.3.411
-docker run --name vitis_ai_quantize_compile --rm xilinx/vitis-ai:1.3.411 bash
+docker run -dit --name vitis_ai_quantize_compile --rm -e PATH="/opt/vitis_ai/conda/bin:/opt/vitis_ai/conda/bin:/opt/vitis_ai/conda/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" -e CONDA_EXE="/opt/vitis_ai/conda/bin/conda" -e CONDA_PREFIX="/opt/vitis_ai/conda" -e CONDA_PYTHON_EXE="/opt/vitis_ai/conda/bin/python" xilinx/vitis-ai:1.3.411 bash
+
 docker cp custom.json vitis_ai_quantize_compile:/home/vitis-ai-user
 docker cp quantize_model.py vitis_ai_quantize_compile:/home/vitis-ai-user
 docker cp ULTRA96V2.dcf vitis_ai_quantize_compile:/home/vitis-ai-user
 docker cp u96v2_mobilenetv2_customcoco/trained_model/mobilenetv2 vitis_ai_quantize_compile:/home/vitis-ai-user
-docker exec vitis_ai_quantize_compile conda activate vitis-ai-tensorflow2
-docker exec vitis_ai_quantize_compile cd /home/vitis-ai-user
-docker exec vitis_ai_quantize_compile python quantize_model.py
-docker exec vitis_ai_quantize_compile vai_c_tensorflow2 -m ./quantized_mobilenetv2_model.h5 -a ./custom.json -o ./compiled_mobilenetv2_model -n mobilenetv2
+
+docker exec -i vitis_ai_quantize_compile bash -c "source /opt/vitis_ai/conda/etc/profile.d/conda.sh && conda activate vitis-ai-tensorflow2 && conda env list && vai_c_tensorflow2 -m /home/vitis-ai-user/quantized_mobilenetv2_model.h5 -a /home/vitis-ai-user/custom.json -o /home/vitis-ai-user/compiled_mobilenetv2_model -n mobilenetv2"
+
 docker cp vitis_ai_quantize_compile:/home/vitis-ai-user/compiled_mobilenetv2_model .
+docker stop vitis_ai_quantize_compile
